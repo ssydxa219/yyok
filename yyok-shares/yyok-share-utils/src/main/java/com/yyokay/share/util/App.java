66bf0db65d88cf562dd2fa30e81cabc01e0e4118
root <ssydxa219@126.com>
2018-11-06 18:22:17 +0800

    init

---
+++ yyok-shares/yyok-share-utils/src/main/java/com/yyokay/share/util/App.java
@@ -0,0 +1,13 @@
+package com.yyokay.share.util;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
