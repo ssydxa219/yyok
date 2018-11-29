4b4aae23c0664f86e193ae97ea51d20546a1f96b
root <ssydxa219@126.com>
2018-11-29 20:58:38 +0800

    init bag data struct

--- yyok-etcs/hadoop/3.1/hadoop-env.sh
+++ yyok-etcs/hadoop/3.1/hadoop-env.sh
@@ -1,4 +1,7 @@
 export JAVA_HOME=/usr/java/jdk1.8.0_192-amd64
 export HADOOP_NAMENODE_OPTS=" -Xms1024m -Xmx1024m -XX:+UseParallelGC"
 export HADOOP_DATANODE_OPTS=" -Xms512m -Xmx512m"
-export HADOOP_LOG_DIR=/data/hadoop/logs
+export HADOOP_HOME=/ddhome/bin/hadoop
+export HADOOP_LOG_DIR=/data/local/hadoop/log
+export HADOOP_PID_DIR=/ddhome/bin/hadoop/tmp
+export HADOOP_CONF_DIR=/ddhome/bin/hadoop/etc/hadoop
