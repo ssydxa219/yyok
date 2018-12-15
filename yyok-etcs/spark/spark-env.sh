61611f6c4db93b85c1c023b3934cad7f5bffcda4
root <ssydxa219@126.com>
2018-12-15 22:52:40 +0800

    init bag data architecture struct

--- yyok-etcs/spark/spark-env.sh
+++ yyok-etcs/spark/spark-env.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 export JAVA_HOME=/usr/java/jdk1.8.0_192-amd64
-export SPARK_MASTER_IP=dda
+export SCALA_HOME=/ddhome/bin/scala
+export HADOOP_HOME=/ddhome/bin/hadoop
+export HADOOP_PROFIX=/ddhome/bin/adoop
+export SPARK_HOME=/ddhome/bin/spark
+export SPARK_LIBRARY_PATH=/ddhome/bin/spark/lib
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export YARN_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export SPARK_DIST_CLASSPATH=$(hadoop classpath)
+export SPARK_MASTER_IP=ddb
 export SPARK_MASTER_PORT=7077
 export SPARK_WORKER_CORES=1
 export SPARK_WORKER_INSTANCES=1
-export SPARK_WORKER_MEMORY=1024
-export HADOOP_HOME=/ddhome/bin/hadoop
-export HADOOP_CONF_DIR=/ddhome/bin/hadoop/etc/hadoop
-#export SPARK_DAEMON_JAVA_OPTS="-Dspark.deploy.recoveryMode=ZOOKEEPER -Dspark.deploy.zookeeper.url=hadoop1:2181,hadoop2:2181,hadoop3:2181,hadoop4:2181 -Dspark.deploy.zookeeper.dir=/spark"
+export SPARK_WORKER_MEMORY=1024m
+export SPARK_DRIVER_MEMORY=1024m
+export SPARK_WORKER_PORT=7078
+export SPARK_LAUNCH_WITH_SCALA=0
+#export SPARK_EXECUTOR_MEMORY=512M
+export SPARK_MASTER_WEBUI_PORT=18080
+export SPARK_WORKER_DIR=/data/local/spark/work
+export SPARK_LOG_DIR=/data/local/spark/logs
+export SPARK_PID_DIR='/ddhome/bin/spark/run'
